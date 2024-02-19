92f40403dc9cfc2604101f9570613852d5bf4fd4
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-02-19 13:09:33 +0100

    Add RateProvider plus tests

---
+++ apps/api/src/Commerce/Core/Price/RateProvider.ts
@@ -0,0 +1,54 @@
+import { ConditionTestDataKeyNotFoundException } from "../../../Helper/Condition/Exception/ConditionTestDataKeyNotFoundException";
+import { ProductConditionBuilder } from "../Product/Condition/ProductConditionBuilder";
+import { ProductItem } from "../Product/Item/ProductItem";
+import { Rate } from "./Rate";
+
+export enum RateListType {
+	BASE = "base",
+	ADDON = "addon"
+}
+
+export abstract class RateProvider {
+	protected name: string;
+	protected type: string;
+	public conditions: ProductConditionBuilder;
+
+	public constructor(name: string) {
+		this.name = name;
+		this.type = RateListType.BASE;
+		this.conditions = new ProductConditionBuilder();
+	}
+
+	public getName(): string {
+		return this.name;
+	}
+
+	public getType(): string {
+		return this.type;
+	}
+
+	public setType(type: RateListType): RateProvider {
+		this.type = type;
+		return this;
+	}
+
+	public test(productItem: ProductItem): boolean {
+		let applicable = false;
+		try {
+			applicable = this.conditions.testOnItem(productItem);
+		} catch (error) {
+			if (error instanceof ConditionTestDataKeyNotFoundException) {
+				applicable = false;
+			} else {
+				throw error;
+			}
+		}
+		return applicable;
+	}
+
+	public abstract getRate(productItem: ProductItem): Rate;
+
+	public static sortByType(_any: any) {
+		throw new Error("Method not implemented.");
+	}
+}
---
+++ apps/api/tests/rateprovider.test.ts
@@ -0,0 +1,66 @@
+import { describe, expect, test } from "vitest";
+import { RateListType, RateProvider } from "../src/Commerce/Core/Price/RateProvider";
+import { Rate } from "../src/Commerce/Core/Price/Rate";
+import { ProductItem } from "../src/Commerce/Core/Product/Item/ProductItem";
+
+class ProviderTest extends RateProvider {
+	public getRate(productItem: ProductItem): Rate {
+		return new Rate(productItem.getAttribute("test-attribute") as number * 2);
+	}
+	constructor(name: string) {
+		super(name);
+	}
+}
+
+let provider: RateProvider;
+let item: ProductItem;
+
+describe("Test creating RateProvider", () => {
+	test("with valid params", () => {
+		provider = new ProviderTest("test");
+		expect(provider).toBeInstanceOf(RateProvider);
+	});
+
+	test("getting name", () => {
+		provider = new ProviderTest("slightly-longer-test-name with spaces");
+		expect(provider.getName()).toBe("slightly-longer-test-name with spaces");
+	});
+
+	test("changing type", () => {
+		provider = new ProviderTest("test");
+		provider.setType(RateListType.ADDON);
+		expect(provider.getType()).toBe(RateListType.ADDON);
+	});
+});
+
+describe("Test test function", () => {
+	test("with applicable condition", () => {
+		provider = new ProviderTest("test");
+		provider.conditions.addCondition("item.attributes.test-attribute", ">", 0);
+
+		item = new ProductItem("foo", "bar");
+		item.setAttribute("test-attribute", 5);
+
+		expect(provider.test(item)).toBe(true);
+	});
+
+	test("with inapplicable condition", () => {
+		provider = new ProviderTest("test");
+		provider.conditions.addCondition("item.attributes.test-attribute", ">", 0);
+
+		item = new ProductItem("foo", "bar");
+		item.setAttribute("test-attribute", 0);
+
+		expect(provider.test(item)).toBe(false);
+	});
+});
+
+
+test("Test getRate function", () => {
+	provider = new ProviderTest("test");
+
+	item = new ProductItem("foo", "bar");
+	item.setAttribute("test-attribute", 5);
+
+	expect(provider.getRate(item).getValue()).toBe(10);
+});

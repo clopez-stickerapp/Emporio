768753a8087c08bb03d1cbb1c47caf838a3854c5
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-02-23 12:42:32 +0100

    Add QuantityList

---
+++ apps/api/tests/quantity.test.ts
@@ -0,0 +1,58 @@
+import { beforeEach, describe, expect, test } from "vitest";
+import { QuantityList } from "../src/Commerce/Core/Price/QuantityList";
+import { ProductItem } from "../src/Commerce/Core/Product/Item/ProductItem";
+
+let quantityList: QuantityList;
+
+describe("QuantityList", () => {
+	// Extend the class to test protected attribute
+	class QuantityListExtension extends QuantityList {
+		public getMinQuantity(): number {
+			return this.minQuantity;
+		}
+	}
+
+	test("Test getName", () => {
+		quantityList = new QuantityList("test", [1, 2, 3]);
+		expect(quantityList.getName()).toBe("test");
+	});
+
+	test("Test setMinQuantity", () => {
+		quantityList = new QuantityListExtension("test", [1, 2, 3]);
+		quantityList.setMinQuantity(1);
+		expect((quantityList as QuantityListExtension).getMinQuantity()).toBe(1);
+	});
+
+	test("Test getQuantities", () => {
+		quantityList = new QuantityList("test", [1, 2, 3]);
+		expect(quantityList.getQuantities()).toEqual([1, 2, 3]);
+	});
+
+	test("Test testOnItem", () => {
+		quantityList = new QuantityList("test", [1, 2, 3]);
+		quantityList.conditions.addCondition("item.attributes.material", "==", "white");
+
+		const item = new ProductItem("foo", "bar");
+		expect(quantityList.testOnItem(item)).toBe(false);
+
+		item.setAttribute("material", "white");
+		expect(quantityList.testOnItem(item)).toBe(true);
+	});
+
+	test("Test test", () => {
+		quantityList = new QuantityList("test", [1, 2, 3]);
+		quantityList.setMinQuantity(1);
+		expect(quantityList.test(2)).toBe(true);
+		expect(quantityList.test(1)).toBe(true);
+		expect(quantityList.test(0)).toBe(false);
+	});
+
+	test("Test sortByMinQuantity", () => {
+		const list1 = new QuantityList("test", [1, 2, 3]).setMinQuantity(3);
+		const list2 = new QuantityList("test", [1, 2, 3]).setMinQuantity(1);
+		const list3 = new QuantityList("test", [1, 2, 3]).setMinQuantity(2);
+
+		const sorted = QuantityList.sortByMinQuantity([list1, list2, list3]);
+		expect(sorted).toEqual([list2, list3, list1]);
+	});
+});

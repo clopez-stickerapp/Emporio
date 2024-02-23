a4992955df3d899745ec65f081983d78ec08a17e
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-02-23 08:36:51 +0100

    Add ProductPriceProvider

---
+++ apps/api/src/Commerce/Core/Price/ProductPriceProvider.ts
@@ -0,0 +1,13 @@
+import { ProductItem } from "../Product/Item/ProductItem";
+import { Currencies } from "../Currency/Currency";
+import { Price } from "./Price";
+
+export abstract class ProductPriceProvider {
+	public constructor(protected name: string) {}
+
+	public abstract calculatePrice(productItem: ProductItem, units: number, currency: Currencies): Price;
+
+	public getName(): string {
+		return this.name;
+	}
+}

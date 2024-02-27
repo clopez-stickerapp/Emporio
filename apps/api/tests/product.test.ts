8ab91bae0ce518b1e7f4f41cf371e52dab4882bd
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-02-27 10:42:33 +0100

    Add product tests part 2

--- apps/api/tests/product.test.ts
+++ apps/api/tests/product.test.ts
@@ -15,6 +15,7 @@ import { ProductPriceProvider } from "../src/Commerce/Core/Price/ProductPricePro
 import { Currencies } from "../src/Commerce/Core/Currency/Currency";
 import { Price } from "../src/Commerce/Core/Price/Price";
 import { ProductQuantityListCollection } from "../src/Commerce/Core/Price/ProductQuantityListCollection";
+import { Condition } from "../src/Helper/Condition/Condition";
 
 let value: ProductDynamicValue;
 let item: ProductItem;
@@ -270,7 +271,12 @@ describe("ProductFamily", () => {
 	});
 });
 
-let product: Product;
+class DummyProduct extends Product {
+	public getConditions(): Condition[]{
+		return Object.values(this.conditions.getConditions()) as Condition[];
+	}
+}
+let product: DummyProduct;
 
 describe("Product", () => {
 	beforeEach(() => {
@@ -280,7 +286,7 @@ describe("Product", () => {
 
 		family.addProduct("test-product", "sku123");
 
-		product = service.findProduct("dummy", "test-product");
+		product = new DummyProduct(family, "test-product", "sku123");
 	});
 
 	test("conditions", () => {
@@ -310,4 +316,146 @@ describe("Product", () => {
 
 		expect(product.getAttrMap()).toEqual({ foo: "bar" });
 	});
+
+	describe("withAttrValue", () => {
+		describe("non multivalue attribute", () => {
+			beforeEach(() => {
+				service.registerAttribute(new ProductAttr(ProductAttrValueType.STRING));
+				family.requireAttr("ProductAttr", "foo");
+			});
+
+			test("required and strictly", () => {
+				product.withAttrValue("foo", "bar", true);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(true);
+
+				expect(product.getConditions()).toHaveLength(1);
+				expect(product.getConditions()[0].columnName).toBe("foo");
+				expect(product.getConditions()[0].operator).toBe(ConditionOperators.EQUAL);
+				expect(product.getConditions()[0].conditionValue).toBe("bar");
+			});
+
+			test("required and strictly with array value", () => {
+				product.withAttrValue("foo", ["bar", "baz"], true);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(true);
+
+				expect(product.getConditions()).toHaveLength(1);
+				expect(product.getConditions()[0].columnName).toBe("foo");
+				expect(product.getConditions()[0].operator).toBe(ConditionOperators.IN);
+				expect(product.getConditions()[0].conditionValue).toEqual(["bar", "baz"]);
+			});
+
+			test("required but not strictly", () => {
+				product.withAttrValue("foo", "bar", true, false);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(false);
+
+				expect(product.getConditions()).toHaveLength(1);
+				expect(product.getConditions()[0].columnName).toBe("foo");
+				expect(product.getConditions()[0].operator).toBe(ConditionOperators.EQUAL);
+				expect(product.getConditions()[0].conditionValue).toBe("bar");
+			});
+
+			test("required but not strictly with array value", () => {
+				product.withAttrValue("foo", ["bar", "baz"], true, false);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(false);
+
+				expect(product.getConditions()).toHaveLength(1);
+				expect(product.getConditions()[0].columnName).toBe("foo");
+				expect(product.getConditions()[0].operator).toBe(ConditionOperators.IN);
+				expect(product.getConditions()[0].conditionValue).toEqual(["bar", "baz"]);
+			});
+
+			test("not required", () => {
+				product.withAttrValue("foo", "bar", false);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(false);
+
+				expect(product.getConditions()).toHaveLength(0);
+			});
+
+			test("not required and not strictly", () => {
+				product.withAttrValue("foo", "bar", false, false);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(false);
+
+				expect(product.getConditions()).toHaveLength(0);
+			});
+		});
+
+		describe("multivalue attribute", () => {	
+			beforeEach(() => {
+				service.registerAttribute(new ProductAttr(ProductAttrValueType.STRING, true));
+				family.requireAttr("ProductAttr", "foo");
+			});
+
+			test("required and strictly", () => {
+				product.withAttrValue("foo", "bar", true);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(true);
+
+				expect(product.getConditions()).toHaveLength(1);
+				expect(product.getConditions()[0].columnName).toBe("foo");
+				expect(product.getConditions()[0].operator).toBe(ConditionOperators.EQUAL);
+				expect(product.getConditions()[0].conditionValue).toEqual("bar");
+			});
+
+			test("required and strictly with array value", () => {
+				product.withAttrValue("foo", ["bar", "baz"], true);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(true);
+
+				expect(product.getConditions()).toHaveLength(1);
+				expect(product.getConditions()[0].columnName).toBe("foo");
+				expect(product.getConditions()[0].operator).toBe(ConditionOperators.EQUAL);
+				expect(product.getConditions()[0].conditionValue).toEqual(["bar", "baz"]);
+			});
+	
+			test("required but not strictly", () => {
+				product.withAttrValue("foo", "bar", true, false);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(false);
+
+				expect(product.getConditions()).toHaveLength(1);
+				expect(product.getConditions()[0].columnName).toBe("foo");
+				expect(product.getConditions()[0].operator).toBe(ConditionOperators.IN);
+				expect(product.getConditions()[0].conditionValue).toEqual("bar");
+			});
+
+			test("required but not strictly with array value", () => {
+				product.withAttrValue("foo", ["bar", "baz"], true, false);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(false);
+
+				expect(product.getConditions()).toHaveLength(2);
+				expect(product.getConditions()[0].columnName).toBe("foo");
+				expect(product.getConditions()[0].operator).toBe(ConditionOperators.IN);
+				expect(product.getConditions()[0].conditionValue).toEqual("bar");
+
+				expect(product.getConditions()[1].columnName).toBe("foo");
+				expect(product.getConditions()[1].operator).toBe(ConditionOperators.IN);
+				expect(product.getConditions()[1].conditionValue).toEqual("baz");
+			});
+	
+			test("not required", () => {
+				product.withAttrValue("foo", "bar", false);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(false);
+
+				expect(product.getConditions()).toHaveLength(0);
+			});
+
+			test("not required and not strictly", () => {
+				product.withAttrValue("foo", "bar", false, false);
+				expect(product.isAttrRecommendedFor("foo")).toBe(true);
+				expect(product.isAttrStrictlyRequiredFor("foo")).toBe(false);
+
+				expect(product.getConditions()).toHaveLength(0);
+			});
+		});
+	});
+
+
 });

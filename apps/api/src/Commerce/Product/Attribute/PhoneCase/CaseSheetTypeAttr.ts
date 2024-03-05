9fd675f7145aed98fb1b7b49c05af603875e67b8
belissabanovic <belis.sabanovic@stickerapp.com>
2024-03-05 10:42:00 +0100

    Add PhoneCaseProductFamily

--- apps/api/src/Commerce/Product/Attribute/PhoneCase/CaseSheetTypeAttr.ts
+++ apps/api/src/Commerce/Product/Attribute/PhoneCase/CaseSheetTypeAttr.ts
@@ -2,9 +2,13 @@ import { ProductAttr } from "../../../Core/Product/Attribute/ProductAttr";
 import { ProductAttrValueType } from "../../../Core/Product/Attribute/ProductAttrValueType";
 
 export class CaseSheetTypeAttr extends ProductAttr {
+	public static readonly ALIAS = 'sheet_type';
+
+	public static readonly CASE = "case";
+
 	public constructor() {
 		super( ProductAttrValueType.STRING );
 
-		this.addAttrValue( "case" );
+		this.addAttrValue( CaseSheetTypeAttr.CASE );
 	}
 }

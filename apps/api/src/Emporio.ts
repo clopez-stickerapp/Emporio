6e4b195da7227b11daddb4ab443fb3c667fcf3bf
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-03-11 13:21:30 +0100

    Add getPriceList function

--- apps/api/src/Emporio.ts
+++ apps/api/src/Emporio.ts
@@ -27,4 +27,22 @@ export class Emporio {
 
 		return price;
 	}
+
+	public getPriceList(productItem: ProductItem, lang: string, inclVat: boolean): Price[] {
+		let productFamily = this.productService.retrieveProductFamily(productItem.getProductFamilyName());
+		let minQuantity = productFamily.getMinimumQuantity(productItem) ?? 1;
+		let steps = productFamily.getProductQuantityListCollection()?.getQuantityStepsFor(productItem, minQuantity) ?? [];
+
+		let units = productItem.getUnits() / (productItem.getAttribute<number>("quantity") ?? 1);
+
+		steps = steps.map(step => {
+			return step * units;
+		});
+
+		let prices = steps.map(step => {
+			return this.calculatePrice(productItem, step, lang, inclVat);
+		});
+
+		return prices;
+	}
 }

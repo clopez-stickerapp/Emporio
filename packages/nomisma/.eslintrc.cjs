a829d86e0c7b1292513034ef37a2545646026ceb
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-05-28 08:42:28 +0200

    Delete old nomisma dummy package

--- packages/nomisma/.eslintrc.cjs
+++
@@ -1,5 +0,0 @@
-/** @type { import("eslint").Linter.FlatConfig } */
-module.exports = {
-  root: true,
-  extends: ['@stickerapp-org/eslint-config'],
-};
--- packages/nomisma/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from '@stickerapp-org/tailwind-config';
-
-/** @type {import('tailwindcss').Config} */
-const config = {
-  ...baseConfig,
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-};
-
-export default config;

d5bbb1f35b1cfc42479b270b6141ed4a434d2e49
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-06-11 09:45:56 +0200

    Fix rounding of prices for non-library stickers

--- apps/api/src/app/prices/routes/root.ts
+++ apps/api/src/app/prices/routes/root.ts
@@ -5,6 +5,7 @@ import { ProductItem } from '$/Commerce/Core/Product/Item/ProductItem';
 import { formatCurrency } from '$/Commerce/Core/Currency/Currency';
 import { getLocale } from '$/Commerce/Core/Localization/Locale';
 import { getPriceListSchema, getPricesSchema } from '../schema';
+import { CustomStickerFamily } from '$/Commerce/Product/Family/CustomStickerFamily';
 
 export default async function (fastify: FastifyInstance) {
 	const f = fastify.withTypeProvider<TypeBoxTypeProvider>();
@@ -21,10 +22,12 @@ export default async function (fastify: FastifyInstance) {
 		const quantity = item.getAttribute<number>('quantity') ?? 1;
 
 		const priceDTO = await emporio.calculatePrice(item, quantity, request.query.lang, request.query.incVat)
+
+		const maxDecimals = item.getProductName() == CustomStickerFamily.PRODUCT_LIBRARY_DESIGN ? 2 : 0;
 		const unitPriceFormatted = formatCurrency(priceDTO.unitPrice, {currency: priceDTO.price.currency, locale: getLocale(request.query.lang), minorIfBelowOne: true});
 
 		return {
-			"price": formatPrice(priceDTO.price, request.query.lang, 2),
+			"price": formatPrice(priceDTO.price, request.query.lang, maxDecimals),
 			"unitPrice": priceDTO.unitPrice,
 			unitPriceFormatted,
 			quantity,
@@ -42,10 +45,12 @@ export default async function (fastify: FastifyInstance) {
 
 		let prices = await emporio.getPriceList(item, request.query.lang, request.query.incVat)
 
+		const maxDecimals = item.getProductName() == CustomStickerFamily.PRODUCT_LIBRARY_DESIGN ? 2 : 0;
+
 		//for each price step in prices, format the price
 		let formattedPrices = prices.map(priceStep => {
 			return {
-				price: formatPrice(priceStep.price, request.query.lang, 2),
+				price: formatPrice(priceStep.price, request.query.lang, maxDecimals),
 				unitPrice: priceStep.unitPrice,
 				quantity: priceStep.quantity
 			}

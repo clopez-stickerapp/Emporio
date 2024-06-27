77e15471c2fcfe7f254a8749ca099c01dc439de9
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-06-27 13:17:34 +0200

    Use attribute constants for unit types

--- apps/api/src/product/unit-type/PerPiece.ts
+++ apps/api/src/product/unit-type/PerPiece.ts
@@ -1,9 +1,10 @@
+import { QuantityAttribute } from "$/configuration/attributes/QuantityAttribute";
 import { ProductItem } from "../ProductItem";
 import { UnitType } from "./UnitType";
 
 export class PerPiece extends UnitType {
 	public calculateUnits(productItem: ProductItem): number {
-		const quantity = productItem.getAttribute<number>("quantity");
+		const quantity = productItem.getAttribute<number>(QuantityAttribute.getName());
 
 		if( !quantity ) {
 			throw new Error("Missing required attributes for PerPiece calculation");
--- apps/api/src/product/unit-type/SquareMeter.ts
+++ apps/api/src/product/unit-type/SquareMeter.ts
@@ -1,11 +1,14 @@
+import { HeightAttribute } from "$/configuration/attributes/HeightAttribute";
+import { QuantityAttribute } from "$/configuration/attributes/QuantityAttribute";
+import { WidthAttribute } from "$/configuration/attributes/WidthAttribute";
 import { ProductItem } from "../ProductItem";
 import { UnitType } from "./UnitType";
 
 export class SquareMeter extends UnitType {
 	public calculateUnits(productItem: ProductItem): number {
-		const width_mm = productItem.getAttribute<number>("width_mm");
-		const height_mm = productItem.getAttribute<number>("height_mm");
-		const quantity = productItem.getAttribute<number>("quantity");
+		const width_mm = productItem.getAttribute<number>(WidthAttribute.getName());
+		const height_mm = productItem.getAttribute<number>(HeightAttribute.getName());
+		const quantity = productItem.getAttribute<number>(QuantityAttribute.getName());
 
 		if( !width_mm || !height_mm || !quantity ) {
 			throw new Error("Missing required attributes for SquareMeter calculation");

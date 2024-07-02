fa38e296de0cc23cbea1cf61469d0ee9ffe83686
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-07-02 12:40:26 +0200

    Split resolve function for clarity

--- apps/api/src/data/ConditionValueResolver.ts
+++ apps/api/src/data/ConditionValueResolver.ts
@@ -12,6 +12,16 @@ export function resolve(value: ConditionValue | null): ConditionValue | null {
 	let result: ConditionValue | null = value;
 
 	if (typeof value === "string" && value.charAt(0) === "{") {
+		result = resolveString(value);
+	} else if (Array.isArray(value) && value.length > 0) {
+		result = resolveArray(value);
+	}
+
+	return result;
+}
+
+function resolveString(value: string): ConditionValue {
+	if (value.charAt(0) === "{") {
 		if (stripBrackets(value) in magicList) {
 			console.debug("Replacing magic value:", stripBrackets(value), "...")
 			return magicList[stripBrackets(value)];
@@ -19,20 +29,22 @@ export function resolve(value: ConditionValue | null): ConditionValue | null {
 			console.trace("Magic value not found: " + stripBrackets(value));
 			// throw new Error("Magic list not found: " + stripBrackets(value));
 		}
-	} else if (Array.isArray(value) && value.length > 0) {
-		const resultArray: string[] = [];
-		for (const item of value) {
-			let resolvedResult = resolve(item);
+	}
 
-			if (typeof resolvedResult === "string") {
-				resultArray.push(resolvedResult);
-			} else if (Array.isArray(resolvedResult)) {
-				resultArray.push(...resolvedResult);
-			} else {
-				throw new Error("Array elements must be strings");
-			}
+	return value;
+}
+
+function resolveArray(value: string[]): string[] {
+	const result: string[] = [];
+
+	for (const item of value) {
+		if (typeof item === "string") {
+			result.push(item);
+		} else if (Array.isArray(item)) {
+			result.push(...resolveArray(item));
+		} else {
+			throw new Error("Array elements must be strings");
 		}
-		result = resultArray;
 	}
 
 	return result;

426ce86119e968cf2af30ff532eaa017ca481249
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-07-02 14:05:19 +0200

    Add support for resolving other attribute values

--- apps/api/src/data/ConditionValueResolver.ts
+++ apps/api/src/data/ConditionValueResolver.ts
@@ -1,12 +1,27 @@
 import { ConditionValue } from "$/conditions/ConditionValue";
-import { DigitalLaminates, DigitalMaterials, LaserLaminates, LaserMaterials } from "$/configuration/attributes/ProductionLineAttribute";
+import { MaxSizes } from "$/configuration/attributes/MaxSizeAttribute";
+import { MaxSizeOtherSideValues } from "$/configuration/attributes/MaxSizeOtherSideAttribute";
+import { DigitalLaminates, DigitalMaterials, LaserLaminates, LaserMaterials, ProductionLines } from "$/configuration/attributes/ProductionLineAttribute";
 
-const magicList: Record<string, string[]> = {
+let magicList: Record<string, ConditionValue> = {
 	LaserMaterials: LaserMaterials,
 	LaserLaminates: LaserLaminates,
 	DigitalMaterials: DigitalMaterials,
 	DigitalLaminates: DigitalLaminates,
 }
+
+// Prefixes the keys of an object with a string, so that they can be used in the magic list
+function prefix(prefix: string, obj: Record<string, ConditionValue>): Record<string, ConditionValue> {
+	return Object.fromEntries(Object.entries(obj).map(([key, value]) => [`${prefix}.${key}`, value]));
+}
+
+magicList = {
+	...magicList,
+	...prefix("MaxSizes", MaxSizes),
+	...prefix("ProductionLines", ProductionLines),
+	...prefix("MaxSizeOtherSideValues", MaxSizeOtherSideValues),
+	...prefix("ProductionLines", ProductionLines),
+};
 
 export function resolve(value: ConditionValue | null): ConditionValue | null {
 	let result: ConditionValue | null = value;
@@ -21,14 +36,16 @@ export function resolve(value: ConditionValue | null): ConditionValue | null {
 }
 
 function resolveString(value: string): ConditionValue {
-	if (value.charAt(0) === "{") {
-		if (stripBrackets(value) in magicList) {
-			console.debug("Replacing magic value:", stripBrackets(value), "...")
-			return magicList[stripBrackets(value)];
-		} else {
-			console.trace("Magic value not found: " + stripBrackets(value));
-			// throw new Error("Magic list not found: " + stripBrackets(value));
-		}
+	if (value.charAt(0) !== "{") {
+		return value;
+	}
+
+	if (stripBrackets(value) in magicList) {
+		console.debug("Replacing magic value:", stripBrackets(value), "...")
+		return magicList[stripBrackets(value)];
+	} else {
+		console.trace("Magic value not found: " + stripBrackets(value));
+		// throw new Error("Magic list not found: " + stripBrackets(value));
 	}
 
 	return value;

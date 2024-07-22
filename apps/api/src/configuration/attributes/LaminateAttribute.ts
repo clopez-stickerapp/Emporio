df36fc65d82b4c85e0bb2b45fd75ffa574e44570
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-07-22 06:20:29 +0200

    Fix laminate values ordering

--- apps/api/src/configuration/attributes/LaminateAttribute.ts
+++ apps/api/src/configuration/attributes/LaminateAttribute.ts
@@ -2,10 +2,9 @@ import { ProductAttr } from "$/product/attribute/ProductAttr";
 import { ProductAttrValueType } from "$/product/attribute/ProductAttrValueType";
 
 export const LaminateValues = {
-	UNCOATED: "uncoated",
 	GLOSSY_UV: "glossy_uv",
-	GLOSSY_UV_12_MIL_HEAVY_DUTY: "glossy_uv_12_mil_heavy_duty",
 	GLOSSY_NO_UV: "glossy_no_uv",
+	GLOSSY_UV_12_MIL_HEAVY_DUTY: "glossy_uv_12_mil_heavy_duty",
 	SOFT_TOUCH: "soft_touch",
 	SATIN_MATTE: "satin_matte",
 	SANDY: "sandy",
@@ -14,6 +13,7 @@ export const LaminateValues = {
 	PEBBLE: "pebble",
 	SUPER_ROUGH: "super_rough",
 	TEXTURED: "textured",
+	UNCOATED: "uncoated",
 	WHITE_WINDOW: "white_window",
 	GLOSSY_THIN: "glossy_thin",
 	GLOSSY_THIN_NO_UV: "glossy_thin_no_uv",

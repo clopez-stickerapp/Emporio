292858772d29663e2a3733e2c37a14e8ebf5ab69
Daniel Jansen <daniel.jansen@stickerapp.com>
2024-08-09 13:43:13 +0200

    Update max size digital to 1200

--- apps/api/src/configuration/Attribute/Sticker/MaxSizeAttribute.ts
+++ apps/api/src/configuration/Attribute/Sticker/MaxSizeAttribute.ts
@@ -5,7 +5,7 @@ export class MaxSizeAttribute extends ProductAttr {
 	public static readonly ALIAS = "max_size_mm";
 
 	public static readonly MAX_SIZE_LASER          = 275;
-	public static readonly MAX_SIZE_DIGITAL        = 1250;
+	public static readonly MAX_SIZE_DIGITAL        = 1200;
 	public static readonly MAX_SIZE_SHEET_LEGACY   = 300;
 	public static readonly MAX_SIZE_ONE_SIDE_LASER = 800;
 	public static readonly MAX_SIZE_ROLL           = 980;

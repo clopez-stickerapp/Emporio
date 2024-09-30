32abe4a90e00cda97d6e28549442b63a8661ddcc
belissabanovic <belis.sabanovic@stickerapp.com>
2024-09-30 15:38:27 +0200

    Add function to fetch families

--- apps/ui/src/lib/api.ts
+++ apps/ui/src/lib/api.ts
@@ -1,5 +1,8 @@
 import { RequestBuilder } from './request-builder';
-import { GetAttributesMapResponse } from '@stickerapp-org/emporio-api-contract';
+import {
+  GetAttributesMapResponse,
+  GetFamiliesResponse,
+} from '@stickerapp-org/emporio-api-contract';
 
 const rb = new RequestBuilder(`http://0.0.0.0:5012`);
 
@@ -7,4 +10,10 @@ export async function getAttributeMap(familyName: string, productName?: string) 
   const url = `/product/attribute-map/${familyName}${productName ? `/${productName}` : ''}`;
   const res = await rb.createRequest().setURL(url).execute(GetAttributesMapResponse);
   return res.attributes;
+}
+
+export async function getFamilies() {
+  const url = '/product/families';
+  const res = await rb.createRequest().setURL(url).execute(GetFamiliesResponse);
+  return res.families;
 }

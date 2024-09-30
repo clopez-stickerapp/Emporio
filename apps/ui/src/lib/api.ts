11910be22b3da92d35ebd7aa2994cc644301ffea
belissabanovic <belis.sabanovic@stickerapp.com>
2024-09-30 08:04:24 +0200

    Add attribute map

--- apps/ui/src/lib/api.ts
+++ apps/ui/src/lib/api.ts
@@ -1,3 +1,10 @@
 import { RequestBuilder } from "./request-builder";
+import { GetAttributesMapResponse } from "@stickerapp-org/emporio-api-contract"
 
 const rb = new RequestBuilder( `http://0.0.0.0:5012` );
+
+export async function getAttributeMap( familyName: string, productName?: string ) {
+	const url = `/product/attribute-map/${ familyName }${ productName ? `/${ productName }` : "" }`;
+	const res = await rb.createRequest().setURL( url ).execute( GetAttributesMapResponse );
+	return res.attributes;
+}
